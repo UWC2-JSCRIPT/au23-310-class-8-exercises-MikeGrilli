ac04e7f1b976c7ff729dbd48bf835817484efb7a
Mike Grilli <grillos80@gmail.com>
2023-12-01 17:48:34 -0800

    Updated Assignment

--- best-books.js
+++ best-books.js
@@ -2,7 +2,8 @@ const formEl = document.getElementById('best-books-form');
 const yearEl = document.getElementById('year');
 const monthEl = document.getElementById('month');
 const dateEl = document.getElementById('date');
-
+const booksContain = document.getElementById('books-container')
+//test
 formEl.addEventListener('submit', function(e) {
   e.preventDefault();
 
@@ -10,5 +11,45 @@ formEl.addEventListener('submit', function(e) {
   const month = monthEl.value;
   const date = dateEl.value;
 
+  const userDate = `${year}-${month}-${date}`
+  const list = 'hardcover-fiction'
+  const API = 't55MZBl7Qp7D9NZWNzYKMrVWfz9n1znP'
+  const url =`https://api.nytimes.com/svc/books/v3/lists/${userDate}/${list}?api-key=${API}&limit=5`
+
   // Fetch bestselling books for date and add top 5 to page
-});
+    fetch(url)
+    .then(res => res.json())
+    .then(res => {
+      const bookListArr = res.results.books
+      console.log(bookListArr)
+      bookListArr.forEach(book => {
+
+        const container = document.querySelector('.container')
+        
+        const img = document.createElement('img')
+        img.src = book.book_image
+        img.style.width = '100%'
+        container.appendChild(img)
+
+        const headline = document.createElement('h1')
+        headline.textContent = book.title
+        container.appendChild(headline)
+
+        const subhead = document.createElement('h4')
+        subhead.textContent = book.author
+        container.appendChild(subhead)
+
+        const description = document.createElement('em')
+        description.textContent = book.description
+        container.appendChild(description)
+        container.style.marginBottom = '`100px'
+
+      });
+    })
+    .catch(err => console.log(err))
+
+  })
+
+  
+
+
---
+++ remove-notice.js
@@ -0,0 +1,6 @@
+const mn = document.getElementById('maintenance-notice')
+
+setTimeout(() => {
+    mn.remove()
+  }, 5000);
+  
